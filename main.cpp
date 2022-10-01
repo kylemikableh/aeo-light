06916d606c3b977621571952cbe03509c7462133
Kyle Mikolajczyk <kjmikolajczyk@wpi.edu>
2022-10-01 19:41:10 -0400

    Fixed auto scale issues

--- main.cpp
+++ main.cpp
@@ -27,6 +27,7 @@
 
 int main(int argc, char *argv[])
 {
+	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling); // Enable proper scaling on modern screens
 	QApplication a(argc, argv);
 
 	a.setOrganizationName("Interdisciplinary Mathematics Institute");
